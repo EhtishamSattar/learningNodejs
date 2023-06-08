9d2f8820dfd73bc7079317c9dfb314fee2785cde
Ehtisham <ehtishamsattar888@gmail.com>
2023-06-08 13:50:01 +0500

    made an Api

--- crud.js
+++ crud.js
@@ -1,5 +1,5 @@
 // GET ,POST ,UPDATE ,DELETE
-const { ObjectID } = require('bson');
+const mongodb=require('mongodb')
 const express=require('express')
 const connectdb=require('./mongodb')
 
@@ -9,6 +9,7 @@ app.use(express.json())
 
 app.get('/',async (req,res)=>{
     const collection=await connectdb();
+    // Gives us an array of objects
     const result=await collection.find().toArray()
     // console.warn("data fethed",data);
     res.send(result);
@@ -17,28 +18,33 @@ app.get('/',async (req,res)=>{
 app.post('/',async (req,res)=>{
     console.warn(req.body);
     const collection=await connectdb();
-    const result=await collection.insertMany([req.body]);
+    // insertMany takes an array of object , so pass the parameters accordingly
+    const result=await collection.insertMany(req.body);
     // console.warn("data inserted",data);
     res.send(result);
     
 
 })
 
-app.put('/',async(req,res)=>{
+app.put('/:id',async(req,res)=>{
     const collection=await connectdb();
-    const result=await collection.update({id:req.params.id},{$set:req.body});
+    id=req.params.id;
+    console.warn(id)
+    const result=await collection.updateOne({_id:new mongodb.ObjectId(id)},{$set:req.body});
     console.warn(result);
+    res.send(result)
     
 
 })
 
+
+// make a delete request like this --> http://localhost:5000/6480f810b0cbe3d8d3db5fd6
 app.delete(`/:id`,async (req,res)=>{
     const collection=await connectdb();
     const id = req.params.id;
-    console.warn(req.params.id)
-    const objectId =  new ObjectID(id);
+    console.warn(req.params.id);
 
-    const result = await collection.deleteOne({ _id: objectId });
+    const result = await collection.deleteOne({_id: new mongodb.ObjectId(id)});
     // id=new ObjectId(req.params.id)
     console.warn(result);
     res.send(result)
